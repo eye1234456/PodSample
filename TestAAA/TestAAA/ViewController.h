67c805da1ff265f8d76ce0e479a57d36aeb19f7e
Flow <flow@ms188.com>
2022-04-26 18:40:39 +0800

    update: 增加调试AAASDK

---
+++ TestAAA/TestAAA/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  TestAAA
+//
+//  Created by Flow on 4/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestAAA/TestAAA/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  TestAAA
+//
+//  Created by Flow on 4/26/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
